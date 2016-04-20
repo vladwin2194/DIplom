8b6a4a35a4d44e202e8114d450c66ef4fcd68ac8
vladwin2194 <vladwin2194@gmail.com>
2016-04-20 09:21:19 +0300

    Сайт до диплома

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="VladKor" />
+    <link href="style/css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="style/css/style.css" rel="stylesheet" type="text/css" />
+	<title>Untitled 2</title>
+</head>
+
+<body>
+
+
+
+</body>
+</html>
